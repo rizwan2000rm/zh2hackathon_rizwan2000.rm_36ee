5115e8b6ac0966b12fc634964c0894ad9aed8a41
Rizwan Memon <rizwan2000.rm@gmail.com>
2021-08-26 12:39:53 +0530

    Enviorment Variables

---
+++ src/axios/index.js
@@ -0,0 +1,14 @@
+// Make account holder for every user
+formID
+
+// Assign a bundle to every account holder
+
+// Get accounts by account holders
+
+// Account To Account Transfer
+
+// For passing the money from us (middleman) to the provider
+
+curl -X 'GET' \
+  'https://fusion.tech{fusion_base_url}/bundles/97400c5f-7af2-48fb-b69b-4ffcab4d851d' \
+  -H 'accept: application/json; charset=utf-8'
